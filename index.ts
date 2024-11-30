4d2dce382f820a16ad44b528ccc36ff2aca92ed2
NarsiBhatii <narsibhati31102@gmail.com>
2024-11-30 22:28:33 +0530

    week 17 postgreSQL

---
+++ index.ts
@@ -0,0 +1,32 @@
+import { Client } from "pg";
+import express from "express";
+
+const port = 3000;
+const app = express();
+app.use(express.json());
+
+const pgClient = new Client("");
+
+pgClient.connect();
+
+app.post("/signup", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const insertQuery = `INSERT INTO users (username, email, password) VALUES ($1, $2,$3);`;
+    const response = await pgClient.query(insertQuery, [
+      username,
+      email,
+      password,
+    ]);
+
+    console.log(response);
+    res.send("user created");
+  } catch {
+    res.send("error while signing up");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
